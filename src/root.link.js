ed7c9d83a851c445cfc074d98ca2bec2b4237145
codewithvk <vivekpatel7202@gmail.com>
2021-10-16 07:36:25 +0530

    Name changes

--- src/root.link.js
+++ src/root.link.js
@@ -6,6 +6,6 @@ export const LAST_NAME = 'Javiya';
 export const ADDRESS = 'Rajkot, Gujrat'
 export const EMAIL_ID = 'work.vivekjaviya@gmail.com';
 export const TWITTER_ID = 'Vk_javiya';
-export const FRONT_PAGE_NAME = 'Vivek';
+export const FRONT_PAGE_NAME = 'Vivek Kumar';
 export const FRONT_PAGE_DESC = 'A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.'
 
